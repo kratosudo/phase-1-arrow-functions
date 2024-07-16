34b9540bab2a9eab8c5e657596f1cf88e62057d1
kibiruki <+>
2024-07-16 03:46:10 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+
+// Function expression called divide
+const divide = function (a, b) {
+  return a / b;
+};
+
+// Arrow function called square
+const square = (x) => {
+  return x * x;
+};
+
+// Arrow function called add
+const add = (a, b) => {
+  return a + b;
+};
+
+module.exports = { divide, square, add };
